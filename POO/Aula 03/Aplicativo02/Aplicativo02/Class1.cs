34e19418bffee43b0c7f3e35bd970210975795d2
jonathanribeiro68238-ai <jonathanribeiro68238@gmail.com>
2026-03-20 20:59:14 -0300

    20/03/26 atividade 3

---
+++ POO/Aula 03/Aplicativo02/Aplicativo02/Class1.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Class1
+{
+    internal class Funcionario
+    {
+        public string nome;
+        public double SalarioBruto;
+        public double imposto;
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += SalarioBruto * porcentagem / 100.0;
+        }
+
+        
+
+    }
+}
---
+++ POO/Aula 03/Aplicativo02/Aplicativo02/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+using Class1;
+
+    
+Funcionario y = new Funcionario();
+
+Console.Write("Digite o nome do funcionario: ");
+y.nome = Console.ReadLine();
+
+Console.Write("Digite o salario bruto do funcionario: ");
+y.SalarioBruto = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor total de imposto a pago pelo funcionario: ");
+y.imposto = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Dados do Funcionario \n Nome do Funcionario = {y.nome} \n Salario Liquido {y.SalarioLiquido()}");
+
+Console.Write("\nDigite a porcentagem de aumento do salario do funcionario: ");
+double porcentagem = double.Parse(Console.ReadLine());
+y.AumentarSalario(porcentagem);
+
+Console.WriteLine($"Salario do funcionario aumentado {porcentagem}%");
+
+Console.WriteLine($"Dados do funcionario atualizados \n Nome do funcionario :{y.nome}\n Salario : {y.SalarioBruto}");
+
+ReadKey();
