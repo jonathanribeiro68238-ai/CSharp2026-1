7c2a66da2331233f11b798ca83f5db53152df430
jonathanribeiro68238-ai <jonathanribeiro68238@gmail.com>
2026-03-27 20:55:55 -0300

    Aula 27/03/26 Aula 06

---
+++ POO/Aula 06/Banco1/Banco1/ContaBancaria.cs
@@ -0,0 +1,67 @@
+﻿namespace Banco1
+{
+    internal class ContaBancaria
+    {
+
+		//Campo
+		private int numero;
+		private string titular;
+		private double saldo;
+		//Propriedades
+		public int Numero
+		{
+			get { return numero; }
+			set { numero = value; }
+		}
+		public string Titular
+		{
+			get { return Titular; }
+			set { Titular = value; }
+		}
+		public double Saldo
+		{
+			get { return Saldo; }
+			set
+			{
+				if (value >  0)
+				{
+					saldo = value;
+				}
+				else
+				{
+					saldo = 0;
+				}
+			}
+		}
+
+		public ContaBancaria (int numero, string titular)
+		{
+			Numero = numero;
+			Titular = titular;
+			Saldo = saldo;
+		}
+
+		public ContaBancaria(int numero, string titular, double saldo) : this(numero,titular)
+		{
+			Saldo = saldo;
+		}
+
+		public void Deposito(double quantia)
+		{
+			Saldo -= quantia;
+		}
+
+		public void Dados()
+		{
+            Console.WriteLine($"Dados da conta:\n" + 
+				$"\tConta: {Numero}\n" +
+				$"\tTitular: {Titular}\n" + 
+				$"\tSaldo: R$ {Saldo:C}");
+		}
+
+
+
+
+
+	}
+}
---
+++ POO/Aula 06/Banco1/Banco1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Banco1;
+using static System.Console;
+
+Title = "Cadastro de conta";
+ContaBancaria conta;
+
+//Entrada de dados
+Write("Entre com o número da conta : ");
+int n = int.Parse(ReadLine());
+Write("Entre com o nome titular da conta: ");
+string nome = ReadLine();
+Write("Deseja fazer depósito inicial (s/n): ");
+char reposta = char.Parse(ReadLine().ToLower());
+
+if(reposta == 's')
+{
+    Write("Entre com o valor de depósito inicial: ");
+    double dep = double.Parse(ReadLine());
+    conta = new ContaBancaria(n, nome, dep);
+}
+else
+{
+    conta = new(n, nome);
+}
+
+conta.Dados();
