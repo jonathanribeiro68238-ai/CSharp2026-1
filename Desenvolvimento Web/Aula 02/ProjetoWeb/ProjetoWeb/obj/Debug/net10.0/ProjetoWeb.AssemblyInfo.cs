054a7eb0285208dba75d9a9bb0eb39a7533117ac
jonathanribeiro68238-ai <jonathanribeiro68238@gmail.com>
2026-05-11 21:49:32 -0300

    Aula 02 continuação

--- Desenvolvimento Web/Aula 02/ProjetoWeb/ProjetoWeb/obj/Debug/net10.0/ProjetoWeb.AssemblyInfo.cs
+++ Desenvolvimento Web/Aula 02/ProjetoWeb/ProjetoWeb/obj/Debug/net10.0/ProjetoWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78e2a0867b26dac5282296584cdb504ad1033e00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cfcbd30abad3c60b6826dcc62c21de506a17773")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
